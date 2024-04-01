d38bfc7ac205600ec32d1d7bdc87a3dc725d91dd
pantspower <hello.kerrin@gmail.com>
2024-03-31 22:32:40 -0400

    made it worse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 
 
 
-$('.faq-question').click(function(){
-    $(this).children("p").slideToggle(500); 
-    $(this).children("#faq-button").toggleClass("closed open");
- });
+$(".faq-question-span").click(function () {
+  $(this).parents(".faq-question").children("p").slideToggle(500);
+  $(this)
+    .parents(".faq-question")
+    .children(".faq-button")
+    .toggleClass("closed open");
+});
 
  $('button#yes').click(function(){
     $('#popup-container').css('display', 'none');
